5fea5710be8d7a8b1e8b2dacd9d9deccf2bab473
mousa-aljasem <mousaaljasem121@gmail.com>
2025-07-25 23:07:27 +0100

    Create youtube-liker.js
    JS script that will auto like all songs on a playlist

---
+++ youtube-liker.js
@@ -0,0 +1,23 @@
+const songs = document.getElementsByTagName("ytmusic-responsive-list-item-renderer")
+
+console.log(`Found ${songs.length} songs.`);
+
+var i = 1;
+Array.from(songs).forEach((song, index) => {
+    setTimeout(() => {
+        const likeButton = song.querySelector("button[aria-label='Like']");
+        const songInfo = song.getElementsByTagName('a'); // 0 == Song Title, 1 == Artist, 2 == Album
+    
+        console.log(i, songInfo[1].innerHTML, "-", songInfo[0].innerHTML, "- isLiked:", likeButton.getAttribute("aria-pressed"))
+    
+        if (likeButton.getAttribute("aria-pressed") === "false") {
+            likeButton.click();
+            console.log("liked");
+        }
+
+        i++;
+        if (i > songs.length) {
+            console.log("------------ COMPLETE ------------");
+        }
+    }, index * 400)
+})
